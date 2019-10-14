8ba4f17cf2dd5bd469f1542e9d2b1bfe7e959a21
19art91 <artjoms.drozdovs@gmail.com>
2019-10-14 22:01:02 +0300

    removed equals/hashcode overriding as not neeeded now

--- src/com/javaguru/shoppinglist/domain/Product.java
+++ src/com/javaguru/shoppinglist/domain/Product.java
@@ -61,19 +61,6 @@ public class Product {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return name.equals(product.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Integer.parseInt(name);
-    }
-
-    @Override
     public String toString() {
         return "Product{" +
                 "id=" + id +
