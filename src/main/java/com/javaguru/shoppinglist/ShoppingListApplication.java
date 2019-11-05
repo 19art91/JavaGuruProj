30fc74a9a3ee64b3a2d532c65ad19ceeaacd8233
19art91 <artjoms.drozdovs@gmail.com>
2019-11-05 23:26:43 +0200

    added AppConfig import

--- src/main/java/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/main/java/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -1,5 +1,6 @@
 package com.javaguru.shoppinglist;
 
+import com.javaguru.shoppinglist.config.AppConfig;
 import com.javaguru.shoppinglist.console.ConsoleUI;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
